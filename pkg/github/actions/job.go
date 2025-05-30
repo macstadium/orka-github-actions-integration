1527bc9fb9898f8e45c153daa8515953fb16e83d
Eric Bellefontaine <15331411+ebellefontaine@users.noreply.github.com>
2025-05-30 12:53:14 -0500

    fix(acquireJobs): fix path for GHES support

--- pkg/github/actions/job.go
+++ pkg/github/actions/job.go
@@ -27,7 +27,14 @@ func (client *ActionsClient) GetAcquirableJobs(ctx context.Context, runnerScaleS
 }
 
 func (client *ActionsClient) AcquireJobs(ctx context.Context, runnerScaleSetId int, messageQueueAccessToken string, requestIds []int64) ([]int64, error) {
-	path := fmt.Sprintf("%s%s/%d/acquirejobs?api-version=6.0-preview", client.actionsServiceUrl, scaleSetEndpoint, runnerScaleSetId)
+	var path string
+	if strings.Contains(client.actionsServiceUrl, "github.com") {
+		// GitHub.com format
+		path = fmt.Sprintf("%s%s/%d/acquirejobs?api-version=6.0-preview", client.actionsServiceUrl, scaleSetEndpoint, runnerScaleSetId)
+	} else {
+		// GitHub Enterprise format
+		path = fmt.Sprintf("%s/%s/%s/acquirejobs?api-version=6.0-preview", client.actionsServiceUrl, scaleSetEndpoint, fmt.Sprint(runnerScaleSetId))
+	}
 
 	body, err := json.Marshal(requestIds)
 	if err != nil {
