f5acdbbee35050a5b6b5688ac9dfaed575654338
Robert Elwell <robert.elwell@gmail.com>
2025-07-11 12:43:49 -0400

    Don't error out when unable to receive message

--- pkg/github/runners/manager.go
+++ pkg/github/runners/manager.go
@@ -140,7 +140,7 @@ func (m *RunnerManager) ProcessMessages(ctx context.Context, handler func(msg *t
 	for {
 		message, err := m.messageQueueManager.ReceiveNextMessage(ctx, m.lastMessageId)
 		if err != nil {
-			return fmt.Errorf("unable to get the next message from the message queue. %w", err)
+			m.logger.Errorf("unable to get the next message from the message queue. %w", err)
 		}
 
 		if message == nil {
