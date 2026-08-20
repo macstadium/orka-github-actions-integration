4f394ae47a908018ca8bef112d523338b22a4b1b
Roy Larsen <roy@larsen.lol>
2026-08-20 16:38:05 -0400

    Remove dead code superseded by the scaleset SDK (#73)
    * Remove dead code after scaleset introduction
    
    * Remove message queue token expire logic

--- pkg/github/actions/errors.go
+++ pkg/github/actions/errors.go
@@ -5,12 +5,7 @@
 package actions
 
 import (
-	"bytes"
-	"encoding/json"
 	"fmt"
-	"io"
-	"net/http"
-	"strings"
 )
 
 type ActionsError struct {
@@ -21,42 +16,4 @@ type ActionsError struct {
 
 func (e *ActionsError) Error() string {
 	return fmt.Sprintf("%v - had issue communicating with Actions backend: %v", e.StatusCode, e.Message)
-}
-
-func ParseActionsErrorFromResponse(response *http.Response) error {
-	if response.ContentLength == 0 {
-		message := "Request returned status: " + response.Status
-		return &ActionsError{
-			ExceptionName: "unknown",
-			Message:       message,
-			StatusCode:    response.StatusCode,
-		}
-	}
-
-	body, err := io.ReadAll(response.Body)
-	if err != nil {
-		return err
-	}
-
-	body = trimByteOrderMark(body)
-	contentType, ok := response.Header["Content-Type"]
-	if ok && len(contentType) > 0 && strings.Contains(contentType[0], "text/plain") {
-		message := string(body)
-		statusCode := response.StatusCode
-		return &ActionsError{
-			Message:    message,
-			StatusCode: statusCode,
-		}
-	}
-
-	actionsError := &ActionsError{StatusCode: response.StatusCode}
-	if err := json.Unmarshal(body, &actionsError); err != nil {
-		return err
-	}
-
-	return actionsError
-}
-
-func trimByteOrderMark(body []byte) []byte {
-	return bytes.TrimPrefix(body, []byte("\xef\xbb\xbf"))
 }
--- pkg/utils/helpers_test.go
+++ pkg/utils/helpers_test.go
@@ -1,7 +1,7 @@
 package utils_test
 
 import (
-	"time"
+	"strconv"
 
 	"github.com/macstadium/orka-github-actions-integration/pkg/utils"
 	. "github.com/onsi/ginkgo/v2"
@@ -9,30 +9,13 @@ import (
 )
 
 var _ = Describe("Utils tests", func() {
-	Context("GetTokenExpirationTime", func() {
-		It("should correctly extract expiration time", func() {
-			token := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2NTA3MDI3NDUsInVzZXJuYW1lIjoiYWRtaW4ifQ.s56k7k89uI3jJ245jB8kF1234567890"
-			expiration, err := utils.GetTokenExpirationTime(token)
-
-			Expect(err).To(BeNil())
-			Expect(expiration).Should(BeTemporally("<", time.Now()))
+	Context("Map", func() {
+		It("should apply the function to every element", func() {
+			Expect(utils.Map([]int{1, 2, 3}, strconv.Itoa)).To(Equal([]string{"1", "2", "3"}))
 		})
 
-		It("should return error for a token with missing expiration claim", func() {
-			token := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFkbWluIn0.s56k7k89uI3jJ245jB8kF1234567890"
-
-			_, err := utils.GetTokenExpirationTime(token)
-
-			Expect(err).To(HaveOccurred())
-			Expect(err.Error()).To(Equal("missing expiration claim in token"))
-		})
-		It("should return error for invalid token", func() {
-			token := "invalid-token"
-
-			_, err := utils.GetTokenExpirationTime(token)
-
-			Expect(err).To(HaveOccurred())
-			Expect(err.Error()).To(Equal("failed to parse jwt token: token contains an invalid number of segments"))
+		It("should return an empty slice for an empty input", func() {
+			Expect(utils.Map([]int{}, strconv.Itoa)).To(BeEmpty())
 		})
 	})
 })
